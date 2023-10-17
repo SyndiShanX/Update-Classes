6907e82575517aa001839a7ad6a0eae318c36f33
SyndiShanX <brianmatthew.1026@gmail.com>
2023-10-17 02:16:38 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Update-Classes
-Updates the Classes in a Theme File
+Updates the Classes in a Theme File using a [Change List](https://github.com/SyndiShanX/Update-Classes/blob/main/Changes.txt)
+
+- Wrap the Keys in Double Quotes
+- Add a Unique Number/String to the Start of each key to prevent Duplicates or they will be Omitted
+- Make Sure all Lines except for the Last Line have a Comma at the End
+
+You can Generate the Numbers Easily using this Site: https://numbergenerator.org/numberlist
+
+## To Run Script:
+
+#### Requirements: Python
+
+* Download the [Script File](https://github.com/SyndiShanX/Update-Classes/blob/main/Update-Classes.py)
+
+* Run `python Update-Classes.py` in Terminal
+
+* The Updated File will be Output to the Current Directory Opened in Terminal as Output.txt
---
+++ Update-Classes.py
@@ -0,0 +1,32 @@
+import json
+import os
+
+fileDir = os.getcwd()
+fileDir = fileDir.replace('\\', '/')
+fileDir = fileDir + '/'
+
+with open(input('Enter the Directory of your Diff File: '), 'r') as rawFile:
+  rawText = " ".join(line.rstrip() for line in rawFile)
+  editedText = '"dummy": "dummy-content",' + rawText
+
+classString = ('{' + rawText + '}')
+classJson = json.loads(classString)
+
+themeDir = input('Enter the Directory of your Theme File: ')
+
+themeFile = open(themeDir, 'r')
+themeText = themeFile.read()
+
+i = 1
+classNum = len(editedText.split('":')) - 2
+
+while i < classNum:
+  class1 = classJson[editedText.split('":')[i].split('"')[3]]
+  class2 = classJson[editedText.split('":')[i + 1].split('"')[3]]
+
+  themeText = themeText.replace(class1, class2)
+
+  i = i + 2
+
+with open(fileDir + 'Output.txt', "w") as outputFile:
+  outputFile.write(themeText)
