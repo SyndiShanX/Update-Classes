81de3a5f6b638fe250a78899a7ee6b71fbc1970b
SyndiShanX <brianmatthew.1026@gmail.com>
2023-10-23 23:42:38 -0400

    Changed the way the Diff File is Structured to make it Simpler

--- README.md
+++ README.md
@@ -1,11 +1,7 @@
 # Update-Classes
 Updates the Classes in a Theme File using a [Change List](https://github.com/SyndiShanX/Update-Classes/blob/main/Changes.txt)
 
-- Wrap the Keys in Double Quotes
-- Add a Unique Number/String to the Start of each key to prevent Duplicates or they will be Omitted
-- Make Sure all Lines except for the Last Line have a Comma at the End
-
-You can Generate the Numbers Easily using this Site: https://numbergenerator.org/numberlist
+Place the Old Class on Line above the New Class (Don't leave Blank Lines)
 
 ## To Run Script:
 
--- Update-Classes.py
+++ Update-Classes.py
@@ -1,28 +1,29 @@
-import json
 import os
 
 fileDir = os.getcwd()
 fileDir = fileDir.replace('\\', '/')
 fileDir = fileDir + '/'
 
-with open(input('Enter the Directory of your Diff File: '), 'r') as rawFile:
-  rawText = " ".join(line.rstrip() for line in rawFile)
-  editedText = '"dummy": "dummy-content",' + rawText
+changesDir = input('Enter the Directory of your Diff File (Leave Blank to use Changes.txt): ')
+if changesDir == '':
+  changesDir = 'Changes.txt'
+  
+with open(changesDir, 'r') as rawFile:
+  rawText = "\n".join(line.rstrip() for line in rawFile).split('\n')
 
-classString = ('{' + rawText + '}')
-classJson = json.loads(classString)
-
-themeDir = input('Enter the Directory of your Theme File: ')
+themeDir = input('Enter the Directory of your Theme File (Leave Blank to use Input.css): ')
+if themeDir == '':
+  themeDir = 'Input.css'
 
 themeFile = open(themeDir, 'r')
 themeText = themeFile.read()
 
-i = 1
-classNum = len(editedText.split('":')) - 2
+i = 0
+classNum = len(rawText) - 1
 
 while i < classNum:
-  class1 = classJson[editedText.split('":')[i].split('"')[3]]
-  class2 = classJson[editedText.split('":')[i + 1].split('"')[3]]
+  class1 = rawText[i]
+  class2 = rawText[i + 1]
 
   themeText = themeText.replace(class1, class2)
 
